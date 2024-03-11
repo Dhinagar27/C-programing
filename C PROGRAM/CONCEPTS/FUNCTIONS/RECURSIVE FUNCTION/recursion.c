f71d99fe54c21d327fc924f9f9a5df3624c35f03
Dhinagar D <117499917+Dhinagar27@users.noreply.github.com>
2024-03-11 07:09:05 -0700

    Add files via upload

---
+++ C PROGRAM/CONCEPTS/FUNCTIONS/RECURSIVE FUNCTION/recursion.c
@@ -0,0 +1,28 @@
+// recursion function in c programing
+/*
+recursion means repeating of a process
+A function thats call itself is known as recursive function
+
+*/
+#include <stdio.h>
+//function declaration
+int factorial(int);
+void main()
+{
+    int number ;
+    printf("Enter the factorial number : ");
+    scanf("%d",&number);
+    printf("Factorial of the Number : %d",factorial(number)); //function calling 
+
+}
+
+//function definition
+int factorial(int number)
+{
+    // it will run untill the number is = 1 
+    if(number<=1)
+    {
+        return 1;
+    }
+    return number*factorial(number-1); //it call itself
+}
