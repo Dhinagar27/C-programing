de4c707f371bdb0817b7fbdb9ae00d08d3a1d9d4
Dhinagar D <117499917+Dhinagar27@users.noreply.github.com>
2024-03-14 07:29:35 -0700

    Add files via upload

---
+++ C PROGRAM/EXAMPLE PROGRAM/example20.c
@@ -0,0 +1,30 @@
+// find the large number in the array
+
+#include<stdio.h>
+void main(){
+    int n,a[100];
+    printf("Enter the limit : ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter the array elements : ");
+        scanf("%d",&a[i]);
+    }
+    
+    //for finding the largest element in the array we should first initialy fix the max value like below
+    int max = a[0];
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i]>max)
+        {
+            max = a[i];
+        }
+    }
+    
+     printf("Max value : %d",max);
+    
+    
+    
+}
